94485e990d0266302f99eaa3a36ec25962556851
KanzakiHMiu <89072078+KanzakiHMiu@users.noreply.github.com>
2023-12-01 15:10:50 +0900

    Add files via upload

---
+++ stl-2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::vector<int> > matrix(3, std::vector<int>(4, 0));
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++){
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    getchar();
+    return 0;
+}
---
+++ stl-3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v1;
+    //输入
+    char c;
+    while(std::cin.get(c) && c != '\n'){
+        if (c != ' '){
+            int x = c - '0';
+            v1.push_back(x);
+        }
+    }
+    std::vector<int> v2;
+    //计算相邻两数的和
+    for (int i = 0; i + 1 < v1.size(); i += 2){
+        v2.push_back(v1[i] + v1[i + 1]);
+    }
+    //输出
+    for (int i = 0; i < v2.size(); i++){
+        std::cout << v2[i] << " ";
+    }
+    getchar();
+    return 0;
+}
