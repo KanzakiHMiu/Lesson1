8d4bae523da817afd6ac9d7c823046988f6cc4d4
KanzakiHMiu <kmyumhsheng@gmail.com>
2023-12-04 12:09:06 +0900

    1

---
+++ stl-12.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <list>
+
+int main()
+{
+    std::list<std::string> l;
+    int N;
+    std::cin >> N;
+    std::string cmd;
+    for (int i = 0; i < N; i++){
+        std::cin >> cmd;
+        if (cmd == "in"){
+            std::string name;
+            std::cin >> name;
+            l.push_back(name);
+        }
+        else if (cmd == "out"){
+            if (!l.empty()){
+                l.pop_front();
+            }
+        }
+        else if (cmd == "q"){
+            if (l.empty()){
+                std::cout << "NULL" << std::endl;
+            }
+            else {
+                std::cout << l.front() << std::endl;
+            }
+        }
+
+    }
+    return 0;
+}
---
+++ stl-13.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+bool isPair(const std::string& s){
+    int len = s.size();
+    if (len % 2){
+        return false;
+    }
+    std::stack<char> stack;
+    for (char c : s){
+        if (c == '(' || c == '[' || c == '{'){
+            stack.push(c);
+        }else {
+            if (stack.empty()){
+                return false;
+            }
+
+            char top = stack.top();
+            if ((c == ')' && top == '(') ||
+                (c == ']' && top == '[') ||
+                (c == '}' && top =='{')){
+                stack.pop();
+            }else {
+                return false;
+            }
+        }
+    }
+    return stack.empty();
+}
+
+int main()
+{
+    int T;
+    std::cin >> T;
+    while (T--){
+        std::string s;
+        std::cin >> s;
+        if (isPair(s))
+            std::cout << "Yes" << std::endl;
+        else
+            std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
